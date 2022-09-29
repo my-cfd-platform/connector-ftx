974ee3579aba11231d0bf70d35d792f09361f804
Maxim Gorin <maksim.g@simple.app>
2022-09-29 21:05:20 +0300

    Fix build

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -2,10 +2,8 @@ mod ftx_ws_client;
 mod models;
 mod error;
 mod event_handler;
-mod ws_connection;
 
 pub use ftx_ws_client::*;
 pub use models::*;
 pub use error::*;
 pub use event_handler::*;
-pub use  ws_connection::*;
